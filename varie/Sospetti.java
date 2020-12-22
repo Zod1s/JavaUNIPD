3a526cdc390fe6427586f8a43180d61f726a152b
Zod1s <lorefrance15@gmail.com>
2020-12-22 16:33:01 +0100

    Varie

---
+++ varie/Sospetti.java
@@ -0,0 +1,43 @@
+public class Sospetti{
+	private String nome;
+	private int[] nSTR;
+	
+	public Sospetti(String name){
+		nome=name;
+		nSTR=new int[0];
+	}
+	
+	public Sospetti(String name, int[] STR){
+		nome=name;
+		if(STR==null || STR.length<1) throw new IllegalArgumentException();
+		nSTR=new int[STR.length];
+		for(int i=0;i<STR.length;i++)
+			nSTR[i]=STR[i];
+	}
+	
+	public void addSTR(int n){
+		nSTR=ArrayUtil.resize(nSTR,nSTR.length+1);
+		nSTR[nSTR.length-1]=n;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public int[] getNstr(){
+		return nSTR;
+	}
+	
+	public boolean verifica(int[] elementi){
+		if(elementi==null || elementi.length<1) throw new IllegalArgumentException();
+	
+		if(elementi.length>nSTR.length)
+			return false;
+		for(int i=0;i<elementi.length;i++){
+			if(elementi[i]!=nSTR[i])
+				return false;
+		}
+		return true;
+		
+	}
+}
