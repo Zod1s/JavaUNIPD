1916049182bd74fbdc8602c9f1215da35fb9f013
Zod1s <lorefrance15@gmail.com>
2020-12-22 16:59:26 +0100

    Varie

---
+++ varie/ArrayUtil.java
@@ -0,0 +1,37 @@
+public class ArrayUtil{
+	public static int[] resize(int[] vecchioArray, int nuovaLunghezza){
+		if(vecchioArray == null || nuovaLunghezza < 0) throw new IllegalArgumentException();
+		int lunghezza = vecchioArray.length;
+		if(nuovaLunghezza < lunghezza)
+			lunghezza = nuovaLunghezza;
+		int[] nuovoArray = new int[nuovaLunghezza];
+		for(int i = 0; i < lunghezza; i++){
+			nuovoArray[i] = vecchioArray[i];
+		}
+		return nuovoArray;
+	}
+	
+	public static Sospetti[] resize(Sospetti[] vecchioArray, int nuovaLunghezza){
+		if(vecchioArray == null || nuovaLunghezza < 0) throw new IllegalArgumentException();
+		int lunghezza = vecchioArray.length;
+		if(nuovaLunghezza < lunghezza)
+			lunghezza = nuovaLunghezza;
+		Sospetti[] nuovoArray = new Sospetti[nuovaLunghezza];
+		for(int i = 0; i < lunghezza; i++){
+			nuovoArray[i] = vecchioArray[i];
+		}
+		return nuovoArray;
+	}
+	
+	public static String[] resize(String[] vecchioArray, int nuovaLunghezza){
+		if(vecchioArray == null || nuovaLunghezza < 0) throw new IllegalArgumentException();
+		int lunghezza = vecchioArray.length;
+		if(nuovaLunghezza < lunghezza)
+			lunghezza = nuovaLunghezza;
+		String[] nuovoArray = new String[nuovaLunghezza];
+		for(int i = 0; i < lunghezza; i++){
+			nuovoArray[i] = vecchioArray[i];
+		}
+		return nuovoArray;
+	}
+}
--- varie/DNAPr.java
+++ varie/DNAPr.java
@@ -1,95 +1,103 @@
 import java.util.*;
 import java.io.*;
-public class DNAPr{
-	public static void main(String[] args){
-		try(FileReader readNomi= new FileReader(args[0]);Scanner nomi=new Scanner(readNomi);FileReader readSequenza= new FileReader(args[1]);Scanner sequenzaScan=new Scanner(readSequenza)){
-			int nSTR=0;
-			int nPersone=0;
-			Scanner riga;
-			String sequenza=sequenzaScan.nextLine();
-			sequenza=sequenza.toUpperCase();
-			char ch;
-			String[] STR=new String[100];
-			Sospetti[] persone=new Sospetti[100];
-			int[] nSTRPresenti;
-			Sospetti persona;
-			boolean trovato=false;
-			
-			if(nomi.hasNextLine())
-				riga=new Scanner(nomi.nextLine());
-			else throw new IllegalArgumentException(); //nel caso in cui il file fosse vuoto	
-			
-			//if(riga.hasNext())
-				riga.next();
-			//else throw new IllegalArgumentException(); //nel caso in cui il file fosse vuoto
-			for(nSTR=0;riga.hasNext();nSTR++){
-				if(STR.length<=nSTR)
-					ArrayUtil.resize(STR,STR.length*2);
-				STR[nSTR]=riga.next().toUpperCase();
-			}
-			for(int i=0;i<nSTR;i++){
-			
-				System.out.println(STR[i]);
-			}
-			
-			for(nPersone=0;nomi.hasNextLine();nPersone++){
-				riga=new Scanner(nomi.nextLine());
-			
-				if(persone.length<=nPersone)
-					ArrayUtil.resize(persone,persone.length*2);
-					
-				//if(riga.hasNext())
-					persona= new Sospetti(riga.next());
-				//else throw new IllegalArgumentException();	
-				persone[nPersone]= persona;  
-				while(riga.hasNext())
-					persone[nPersone].addSTR(Integer.parseInt(riga.next()));
-			}
-			
-			nSTRPresenti= new int[nSTR];
-			while(sequenza.length()>=4){
-				System.out.println(sequenza+"\n");
-				ch=sequenza.charAt(0);
-				for(int j=0;j<nSTR;j++){
-					if(ch==STR[j].charAt(0)){
-						System.out.println(ch+"=="+STR[j].charAt(0)+" ora li confrontano");
-						if(confronto(STR[j],sequenza)){
-							System.out.println("sono uguali, quindi incremento nSTRPresenti[j]"+nSTRPresenti[j]);
-							nSTRPresenti[j]++;
-							System.out.println("ora è uguale a "+nSTRPresenti[j]);
-						}
-					}
-				}
-				sequenza=sequenza.substring(1);
-			}
-			
-			for(int i=0;i<nPersone;i++){
-				if(persone[i].verifica(nSTRPresenti)){
-					trovato=true;
-					System.out.println("Il colpevole e': "+persone[i].getNome());
-				}
-			}
-			
-			if(!trovato)
-				System.out.println("Il colpevole non e' stato trovato");
-		
-		}catch (FileNotFoundException e){
-			System.err.println("File non trovato");
-		}catch (IOException e){
-			System.err.println("Errore nella chiusura del file");
-		}catch (ArrayIndexOutOfBoundsException e){
-			System.err.println("Inserire come parametri i nomi dei file");
-		}catch (IllegalArgumentException e){
-			System.err.println("Contenuto del file errato!");
-	}
-}
-	private static boolean confronto(String s1, String s2){
-		if(s1.length()!=4)
-			s2=s2.substring(0,4);
-		else if(s1.length()!=4)
-			s1=s1.substring(0,4);
-		System.out.println("confronto tra: "+s1 +" e "+s2);
-		return s1.equals(s2);
-	}
 
+public class DNAPr{
+    public static void main(String[] args){
+        try(FileReader readNomi = new FileReader(args[0]); Scanner nomi = new Scanner(readNomi);
+            FileReader readSequenza = new FileReader(args[1]); Scanner sequenzaScan =new Scanner(readSequenza)){
+            int nSTR = 0;
+            int nPersone = 0;
+
+            Scanner riga;
+            String sequenza = sequenzaScan.nextLine();
+            sequenza = sequenza.toUpperCase();
+
+            char ch;
+            String[] str = new String[100];
+            Sospetti[] persone = new Sospetti[100];
+            int[] nSTRPresenti;
+            Sospetti persona;
+            boolean trovato = false;
+			
+            if(nomi.hasNextLine())
+                riga = new Scanner(nomi.nextLine());
+            else throw new IllegalArgumentException(); //nel caso in cui il file fosse vuoto	
+
+            //if(riga.hasNext())
+            riga.next();
+            //else throw new IllegalArgumentException(); //nel caso in cui il file fosse vuoto
+            for(nSTR = 0; riga.hasNext(); nSTR++){
+                if(str.length <= nSTR)
+                    ArrayUtil.resize(str, str.length*2);
+                str[nSTR] = riga.next().toUpperCase();
+            }
+
+            for(int i = 0; i < nSTR; i++){
+                System.out.println(str[i]);
+            }
+
+            for(nPersone = 0; nomi.hasNextLine(); nPersone++){
+                riga = new Scanner(nomi.nextLine());
+
+                if(persone.length<=nPersone)
+                    ArrayUtil.resize(persone,persone.length*2);
+					
+                //if(riga.hasNext())
+                persona = new Sospetti(riga.next());
+                //else throw new IllegalArgumentException();	
+                persone[nPersone] = persona;  
+                while(riga.hasNext())
+                    persone[nPersone].addSTR(Integer.parseInt(riga.next()));
+			}
+
+            nSTRPresenti = new int[nSTR];
+            while(sequenza.length() >= 4){
+                // System.out.println(sequenza+"\n");
+                ch = sequenza.charAt(0);
+                for(int j = 0; j < nSTR; j++){
+                    if(ch == str[j].charAt(0)){
+                        // System.out.println(ch + "==" + str[j].charAt(0) + " ora li confrontano");
+                        if(confronto(str[j], sequenza)){
+                            // System.out.println("sono uguali, quindi incremento nSTRPresenti[j]"+nSTRPresenti[j]);
+                            nSTRPresenti[j]++;
+                            // System.out.println("ora è uguale a "+nSTRPresenti[j]);
+                        }
+                    }
+                }
+                sequenza = sequenza.substring(1);
+            }
+			
+            for(int i = 0; i < nPersone; i++){
+                if(persone[i].verifica(nSTRPresenti)){
+                    trovato = true;
+                    System.out.println("Il colpevole e': "+persone[i].getNome());
+                }
+            }
+
+            if(!trovato)
+                System.out.println("Il colpevole non e' stato trovato");
+
+        }
+        catch (FileNotFoundException e){
+            System.err.println("File non trovato");
+        }
+        catch (IOException e){
+            System.err.println("Errore nella chiusura del file");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.err.println("Inserire come parametri i nomi dei file");
+        }
+        catch (IllegalArgumentException e){
+            System.err.println("Contenuto del file errato!");
+        }
+    }
+
+    private static boolean confronto(String s1, String s2){
+        if(s1.length() != 4)
+            s2 = s2.substring(0,4);
+        else if(s1.length() != 4)
+            s1 = s1.substring(0,4);
+        System.out.println("confronto tra: "+s1 +" e "+s2);
+        return s1.equals(s2);
+    }
 }
