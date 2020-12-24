ad46a87da4475f9b0fdc61abe9f496a0d3180536
Zod1s <75039689+Zod1s@users.noreply.github.com>
2020-12-24 01:38:06 +0100

    Update EmptyQueueException.java

--- ADTs/ADTs/Queue/EmptyQueueException.java
+++ ADTs/ADTs/Queue/EmptyQueueException.java
@@ -1,9 +1,4 @@
 package ADTs.Queue;
-
 public class EmptyQueueException extends RuntimeException{
-
-    /**
-     *
-     */
     private static final long serialVersionUID = 3535124813897934618L;
 }
