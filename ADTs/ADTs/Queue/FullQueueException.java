1fc5234165c61dfe69ca9dc7bc079d9ca30b5eef
Zod1s <75039689+Zod1s@users.noreply.github.com>
2020-12-24 01:38:23 +0100

    Update FullQueueException.java

--- ADTs/ADTs/Queue/FullQueueException.java
+++ ADTs/ADTs/Queue/FullQueueException.java
@@ -1,9 +1,4 @@
 package ADTs.Queue;
-
 public class FullQueueException extends RuntimeException{
-
-    /**
-     *
-     */
     private static final long serialVersionUID = 536583958640050484L;
 }
