b34f6f624135d8f96659a653f261e706f25efa41
Zod1s <75039689+Zod1s@users.noreply.github.com>
2021-02-12 15:18:35 +0100

    Update DNAProfile.java

--- Esercitazione10/DNAProfile.java
+++ Esercitazione10/DNAProfile.java
@@ -74,8 +74,8 @@ public class DNAProfile{
         for (int index = 0; index < (dnaL - strL); index++){
             int curR = 0;
             boolean finish = false;
-            for (int i = index; i <= (dnaL - strL) && !finish; i += strL){ // off-by-one error if < is present in place of <=
-                String sub = dna.substring(i, i + strL);                   // because substring isnt inclusive in the second argument
+            for (int i = index; i < (dnaL - strL + 1) && !finish; i += strL){
+                String sub = dna.substring(i, i + strL);
                 if (str.equals(sub)){
                     curR += 1;
                 }
