cbb987ae3930c77e81de52e2daa9eaf75f721011
Zod1s <75039689+Zod1s@users.noreply.github.com>
2021-02-18 12:01:30 +0100

    Update SortedArrayMap.java

--- Esercitazione11/SortedArrayMap.java
+++ Esercitazione11/SortedArrayMap.java
@@ -18,8 +18,8 @@ public class SortedArrayMap implements SortedMapInterface{
         if (key == null || !(key instanceof Comparable)) throw new IllegalArgumentException();
         Comparable compKey = (Comparable) key;
         int low = 0;
-        int high = size - 1;
-        while (low <= high){
+        int high = size;
+        while (low < high){
             int mid = (low + high) / 2;
             ComparablePair elem = map[mid];
             if (elem.key.equals(key)){
@@ -31,7 +31,7 @@ public class SortedArrayMap implements SortedMapInterface{
                 return elem.value;
             }
             else if (elem.key.compareTo(compKey) > 0){
-                high = mid - 1;
+                high = mid;
             }
             else{
                 low = mid + 1;
